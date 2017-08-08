daffe5eb385684548b3c1c5879fdb23bc0660bc6
JeffRisberg <jrisberg@aol.com>
2017-08-08 00:09:05 -0700

    added vcs file

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ if [[ $environment == 'dev' ]]; then
         # Make sure that docker-machine is running and connected.
         docker-machine start default
         eval $(docker-machine env default)
-        # eval $(aws ecr get-login --profile MainAWS --registry-ids 853301516381 --no-include-email)
+        eval $(aws ecr get-login --profile MainAWS --registry-ids 853301516381 --no-include-email)
 
         export APP_VERSION=$(cd target/classes; cat 'version.properties' | grep "timestamp" | cut -d'=' -f2)
 
